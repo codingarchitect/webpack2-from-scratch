bfb73e74c670c1cd916f4e5efd8031db8fdb54a4
Sendhil Kumar R <sendhil.r@gmail.com>
2017-06-17 11:45:59 +0530

    removed the loader prefixes in the require, Now we can create a bundle using webpack ./entry.js bundle.js --module-bind 'css=style-loader!css-loader' command

--- entry.js
+++ entry.js
@@ -1,3 +1,3 @@
 // See here https://stackoverflow.com/a/34237524 for why we need two loaders
-require("!style-loader!css-loader!./style.css");
+require("./style.css");
 document.write(require("./content.js"));
