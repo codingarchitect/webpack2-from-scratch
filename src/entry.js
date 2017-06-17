b00da04c66f9e2472f54622163d14e9934487136
Sendhil Kumar R <sendhil.r@gmail.com>
2017-06-17 18:57:01 +0530

    Fixed the eslint warning regarding no console

--- src/entry.js
+++ src/entry.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './assets/stylesheets/styles.less'; // This line cost me 2-3 hours of debugging. Without this the styles.css is not emitted by etract text web pack plugin
 import App from './app/App';
 
-console.log(App);
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line react/jsx-filename-extension
