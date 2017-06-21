d8acb3130e39ca409c08a6821407934d0aeb5f93
Sendhil Kumar R <sendhil.r@gmail.com>
2017-06-21 13:23:36 +0530

    Refactored App.jsx and extracted getPageLoader function to be called from AppFactory

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,15 +4,17 @@ import Helmet from 'react-helmet';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import asyncComponent from 'shared/utils/async-component';
 
+function getPageLoaders(store) {
+  return {
+    home: asyncComponent(() => import('./pages/Home/Home').then(module => module.default), { name: 'Home' }),
+    page1: asyncComponent(() => import('./pages/Page1/Page1').then(module => module.default(store)), { name: 'Page1' }),
+    page2: asyncComponent(() => import('./pages/Page2/Page2').then(module => module.default(store)), { name: 'Page2' }),
+    page3: asyncComponent(() => import('./pages/Page3/Page3').then(module => module.default(store)), { name: 'Page3' }),
+  };
+}
+
 export default function AppFactory(store) {
-  const HomePage = asyncComponent(() => import(/* webpackChunkName: "Home" */ './pages/Home/Home')
-    .then(module => module.default), { name: 'Home' });
-  const Page1 = asyncComponent(() => import(/* webpackChunkName: "Page1" */ './pages/Page1/Page1')
-    .then(module => module.default(store)), { name: 'Page1' });
-  const Page2 = asyncComponent(() => import(/* webpackChunkName: "Page2" */ './pages/Page2/Page2')
-    .then(module => module.default(store)), { name: 'Page2' });
-  const Page3 = asyncComponent(() => import(/* webpackChunkName: "Page3" */ './pages/Page3/Page3')
-    .then(module => module.default(store)), { name: 'Page3' });
+  const pageLoaders = getPageLoaders(store);
   const render = () => (<div>
     <Helmet
       titleTemplate="Maginus OMS - %s"
@@ -20,12 +22,13 @@ export default function AppFactory(store) {
     />
     <MuiThemeProvider>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/page1" component={Page1} />
-        <Route path="/page2" component={Page2} />
-        <Route path="/page3" component={Page3} />
+        <Route exact path="/" component={pageLoaders.home} />
+        <Route path="/page1" component={pageLoaders.page1} />
+        <Route path="/page2" component={pageLoaders.page2} />
+        <Route path="/page3" component={pageLoaders.page3} />
       </Switch>
     </MuiThemeProvider>
   </div>);
   return render;
 }
+
