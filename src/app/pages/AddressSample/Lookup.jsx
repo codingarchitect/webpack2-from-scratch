042182e5acfde6dea1d76fc0214d3390cc9585a1
Sendhil Kumar R <sendhil.r@gmail.com>
2017-07-05 14:13:34 +0530

    Added debounce feature to postcode change events

--- src/app/pages/AddressSample/Lookup.jsx
+++ src/app/pages/AddressSample/Lookup.jsx
@@ -1,11 +1,64 @@
 import React from 'react';
+import Rx from 'rxjs';
+import PropTypes from 'prop-types';
 
-const lookup = () =>
-  (<div className="input-group add-on">
-    <input className="form-control srch-term" id="srch-term" type="text" />
-    <div className="input-group-btn">
-      <button className="btn btn-default"><i className="glyphicon glyphicon-search" /></button>
-    </div>
-  </div>);
+import { DebounceTimeForFormFieldChange } from 'shared/constants';
 
-export default lookup;
+class Lookup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lookupValue: props.value,
+      id: props.id,
+      onChange: props.onChange,
+      onBlur: props.onBlur,
+      debouncedValue: props.value,
+    };
+    this.onLookupValueChange$ = new Rx.Subject();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    const lookupThis = this;
+    this.subscription = this.onLookupValueChange$
+      .debounceTime(DebounceTimeForFormFieldChange)
+      .subscribe((debouncedValue) => {
+        this.setState({ debouncedValue });
+        if (lookupThis.state.onChange) lookupThis.state.onChange(debouncedValue);
+      });
+  }
+
+  componentWillUnmount() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  handleChange(e) {
+    const lookupValue = e.target.value;
+    this.setState({ lookupValue });
+    this.onLookupValueChange$.next(lookupValue);
+  }
+
+  render() {
+    const { lookupValue, id, onBlur } = this.state;
+    return (<div className="input-group add-on">
+      <input className="form-control" type="text" onChange={this.handleChange} value={lookupValue} id={id} onBlur={onBlur(id, lookupValue)} />
+      <div className="input-group-btn">
+        <button className="btn btn-default"><i className="glyphicon glyphicon-search" /></button>
+      </div>
+    </div>);
+  }
+}
+
+Lookup.propTypes = {
+  value: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+};
+
+Lookup.defaultProps = {
+  value: '',
+};
+
+export default Lookup;
---
+++ src/app/shared/constants.js
@@ -0,0 +1,3 @@
+export const DebounceTimeForFormFieldChange = 300;
+
+export default {};
